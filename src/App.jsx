b595a468dc21c473108c51c795ffc9a5dc8b9b18
mojet66 <oladosuemmanuel2017@gmail.com>
2026-02-11 12:13:35 +0100

    set up my react app and install tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-green-500 text 4xl">Welcome to the Mojet Quiz App!</p>
+    </>
+  );
+}
+
+export default App;
