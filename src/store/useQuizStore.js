cf5972f9db42a3021c9e0ce1468b5a943e8a2052
mojet66 <oladosuemmanuel2017@gmail.com>
2026-02-23 11:32:49 +0100

    stored fetched questions from api

--- src/store/useQuizStore.js
+++ src/store/useQuizStore.js
@@ -6,25 +6,46 @@ const useQuizStore = create((set) => ({
   currentQuestionIndex: 0,
   score: 0,
   selectedAnswer: null,
+  userAnswers: [],
 
   //Actions to update the state
-  setQuestions: (questions) => set({ questions }),
-  selectAnswer: (answer) => set({ selectedAnswer: answer }),
+  setQuestions: (questions) =>
+    set({
+      questions: Array.isArray(questions) ? questions : [],
+      userAnswers: Array.isArray(questions)
+        ? Array(questions.length).fill(null)
+        : [],
+      currentQuestionIndex: 0,
+    }),
+
+  selectAnswer: (answer) =>
+    set((state) => {
+      const updatedAnswers = [...state.userAnswers];
+      updatedAnswers[state.currentQuestionIndex] = answer;
+      return { userAnswers: updatedAnswers };
+    }),
+
   incrementScore: () =>
     set((state) => ({
       score: state.score + 1,
     })),
+
   nextQuestion: () =>
     set((state) => ({
       currentQuestionIndex: state.currentQuestionIndex + 1,
-      selectedAnswer: null,
     })),
+
+  prevQuestion: () =>
+    set((state) => ({
+      currentQuestionIndex: state.currentQuestionIndex - 1,
+    })),
+
   resetQuiz: () =>
     set({
       questions: [],
       currentQuestionIndex: 0,
       score: 0,
-      selectedAnswer: null,
+      userAnswers: [],
     }),
 }));
 
