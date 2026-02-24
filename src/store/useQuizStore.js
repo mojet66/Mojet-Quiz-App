96e91ef7fe9a2994ce39b243a55ee403c6ba8c6f
mojet66 <oladosuemmanuel2017@gmail.com>
2026-02-24 11:47:26 +0100

    imported persist from zustand middleware to save store state to localStorage

--- src/store/useQuizStore.js
+++ src/store/useQuizStore.js
@@ -1,52 +1,56 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useQuizStore = create((set) => ({
-  // Initial state
-  questions: [],
-  currentQuestionIndex: 0,
-  score: 0,
-  selectedAnswer: null,
-  userAnswers: [],
+const useQuizStore = create(
+  persist((set) => ({
+    // Initial state
+    questions: [],
+    currentQuestionIndex: 0,
+    score: 0,
+    selectedAnswer: null,
+    userAnswers: [],
 
-  //Actions to update the state
-  setQuestions: (questions) =>
-    set({
-      questions: Array.isArray(questions) ? questions : [],
-      userAnswers: Array.isArray(questions)
-        ? Array(questions.length).fill(null)
-        : [],
-      currentQuestionIndex: 0,
-    }),
+    //Actions to update the state
+    setQuestions: (questions) =>
+      set({
+        questions: Array.isArray(questions) ? questions : [],
+        userAnswers: Array.isArray(questions)
+          ? Array(questions.length).fill(null)
+          : [],
+        currentQuestionIndex: 0,
+      }),
 
-  selectAnswer: (answer) =>
-    set((state) => {
-      const updatedAnswers = [...state.userAnswers];
-      updatedAnswers[state.currentQuestionIndex] = answer;
-      return { userAnswers: updatedAnswers };
-    }),
+    selectAnswer: (answer) =>
+      set((state) => {
+        const updatedAnswers = [...state.userAnswers];
+        updatedAnswers[state.currentQuestionIndex] = answer;
+        return { userAnswers: updatedAnswers };
+      }),
 
-  incrementScore: () =>
-    set((state) => ({
-      score: state.score + 1,
-    })),
+    incrementScore: () =>
+      set((state) => ({
+        score: state.score + 1,
+      })),
 
-  nextQuestion: () =>
-    set((state) => ({
-      currentQuestionIndex: state.currentQuestionIndex + 1,
-    })),
+    nextQuestion: () =>
+      set((state) => ({
+        currentQuestionIndex: state.currentQuestionIndex + 1,
+      })),
 
-  prevQuestion: () =>
-    set((state) => ({
-      currentQuestionIndex: state.currentQuestionIndex - 1,
-    })),
+    prevQuestion: () =>
+      set((state) => ({
+        currentQuestionIndex: state.currentQuestionIndex - 1,
+      })),
 
-  resetQuiz: () =>
-    set({
-      questions: [],
-      currentQuestionIndex: 0,
-      score: 0,
-      userAnswers: [],
-    }),
-}));
+    resetQuiz: () =>
+      set({
+        questions: [],
+        currentQuestionIndex: 0,
+        score: 0,
+        userAnswers: [],
+      }),
+  })),
+  { name: "quiz-storage" },
+);
 
 export default useQuizStore;
