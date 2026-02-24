dcfb0021deadbfade831fa35406a454c0d7cab41
mojet66 <oladosuemmanuel2017@gmail.com>
2026-02-24 18:11:45 +0100

    added history page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Quizpage from "./pages/Quizpage";
 import Homepage from "./pages/Homepage";
 import Resultpage from "./pages/Resultpage";
+import Historypage from "./pages/Historypage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/quiz" element={<Quizpage />} />
         <Route path="/results" element={<Resultpage />} />
+        <Route path="/history" element={<Historypage />} />
       </Routes>
     </>
   );
