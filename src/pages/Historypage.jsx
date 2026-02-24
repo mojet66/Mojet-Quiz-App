86d6764ade72d49f413b18ccfa0176869e851635
mojet66 <oladosuemmanuel2017@gmail.com>
2026-02-24 18:15:44 +0100

    created History page

---
+++ src/pages/Historypage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Historypage = () => {
+  return (
+    <div>
+      <h1 className="mt-10 font-bold text-3xl text-center">Quiz History</h1>
+      <p className="mt-4 text-gray-500 text-center">
+        Your past quiz attempts will be displayed here.
+      </p>
+    </div>
+  );
+};
+
+export default Historypage;
